869462f3024dd0f43c031d45b75a60352fa945cf
Sven Bock <sb@generationrobots.com>
2016-06-26 16:12:59 +0200

    added object reco behavior and subscription master

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ import subprocess
 from vision_definitions import kQQVGA,kVGA,kQVGA,k16VGA,kRGBColorSpace,kBGRColorSpace,kDepthColorSpace
 import Image
 import numpy as np
-
+from subscriptionMaster import  SubscriptionMaster
 import classify_image as classifier
+from __builtin__ import True
 IP = "max.local"
 PORT = 9559
-
-# from subscriptionMaster import  SubscriptionMaster
-# sm = SubscriptionMaster(IP,PORT)
+import time
 
 
 class ImageManager():
-    def __init__(self,ip,port):
+    def __init__(self,ip,port,img_name):
+        self.img_name=img_name
         self.videoDevice = ALProxy("ALVideoDevice", ip,port)
-        self.sub_name = "hackathon2016TopCam2"
+        self.sub_name = "hackathon2016TopCam5"
         self.clean_subscribers(self.sub_name)
+        self.tts = ALProxy("ALTextToSpeech",ip,port)
+        self.sm = SubscriptionMaster(ip,port)
+        self.do_stuff = False
+        self.sm.subscribe("Hackathon/event1",self.classifyImageLoop)
+        
         
     def subscribe(self):
-        self.sub_name =  self.videoDevice.subscribeCamera(self.sub_name,0,kQVGA,kRGBColorSpace,5)
+        self.sub_name =  self.videoDevice.subscribeCamera(self.sub_name,1,kQVGA,kRGBColorSpace,5)
 
     def unsubscribe(self):
         self.videoDevice.unsubscribe(self.sub_name)
@@ -34,37 +39,55 @@ class ImageManager():
                 pass
             
     def getAndSaveImage(self,path):
+        time.sleep(2)
         img_array = self.videoDevice.getImageRemote(self.sub_name)
         width = img_array[0]
         height = img_array[1]
         
         frame=np.asarray(bytearray(img_array[6]), dtype=np.uint8)
         frame=frame.reshape((height,width,3))
+        frame = frame[height/5:height-height/5,width/4:width-width/4] 
         im = Image.fromarray(frame)
         im.save(path)
- 
-
-if __name__ == "__main__":
-    im = ImageManager(IP,PORT)
-    print "Subscribe"
-    im.subscribe()
-    img_name = "image.jpg"
-    while True:
+        
+    def classifyImageLoop(self,value):
+        if self.do_stuff is False:
+            self.do_stuff = True
+            
+    def doStuff(self):
         print "getting image"
-        im.getAndSaveImage(img_name)
+        im.getAndSaveImage(self.img_name)
         
         # classify image
         print "getting classification"
-        res = classifier.run_inference_on_image(img_name)[0]
+#         try:
+        res = classifier.run_inference_on_image(self.img_name)[0]
+#         except Exception as exc:
+#             print "no valid result"
+#             print exc
+#             self.tts.say("HAHAHAHAH")
+#             return
         print res
         
         if type(res) == dict:
-            tts = ALProxy("ALTextToSpeech",IP,PORT)
             label = res["label_name"].split(",")[0]
-            tts.say("I have recognized a "+label+" with a probability of" + str(int(res["score"]*100)) + " percent")
+            self.tts.say("I have recognized a "+label+" with a probability of" + str(int(res["score"]*100)) + " percent")
+            self.sm.memory.raiseEvent("Hackathon/event2",1)
         else:
             print "result type is not a dict"
-            break
+ 
+
+if __name__ == "__main__":
+    img_name = "image.jpg"
+    im = ImageManager(IP,PORT,img_name)
+    print "Subscribe"
+    im.subscribe()
+    while True:
+        if im.do_stuff:
+            im.doStuff()
+            im.do_stuff = False
+        else:
+            time.sleep(0.1)
     im.unsubscribe()
     
     
---
+++ subscriptionMaster.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+
+########################################################################### 
+# This software is graciously provided by HumaRobotics 
+# under the Simplified BSD License on
+# github: git@www.humarobotics.com:baxter_tasker
+# HumaRobotics is a trademark of Generation Robots.
+# www.humarobotics.com 
+
+# Copyright (c) 2015, Generation Robots.
+# All rights reserved.
+# www.generationrobots.com
+#   
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice,
+#  this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation 
+#  and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS 
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THE POSSIBILITY OF SUCH DAMAGE. 
+# 
+# The views and conclusions contained in the software and documentation are 
+# those of the authors and should not be interpreted as representing official 
+# policies, either expressed or implied, of the FreeBSD Project.
+#
+#############################################################################
+
+
+import qi
+import time
+
+class SubscriptionMaster(object):
+    def __init__(self,ip,port):
+        connection_url = "tcp://" + ip + ":" + str(port)
+        app = qi.Application(["SubscriptionMaster", "--qi-url=" + connection_url])
+        super(SubscriptionMaster, self).__init__()
+        app.start()
+        self.memory = app.session.service("ALMemory")
+        self.callback = None
+        self.subscriber = {}
+        
+    def subscribe(self,topic,callback):
+        subscriber = self.memory.subscriber(topic)
+        subscriber.signal.connect(self.__cb)
+        id = subscriber.signal.connect(callback)
+        print "subscribed to event",topic
+        self.subscriber[id] = subscriber
+        return id
+    
+    def __cb(self,value):
+        pass
+        
+    def unsubscribe(self,id):
+        self.subscriber[id].signal.disconnect(id)
+        print "unsubscribed"
+        
+class CustomClass():
+    def __init__(self):
+        
+        self.sm = SubscriptionMaster("localhost",9559)
+        self.id = self.sm.subscribe("myEvent", self.callback)
+        self.id2 = self.sm.subscribe("myEvent2", self.callback)
+        
+    def callback(self,value):
+        print"outer callback", value
+            
+    def unsubscribe(self):
+        self.sm.unsubscribe(self.id)
+        self.sm.unsubscribe(self.id2)
+if __name__ == "__main__":
+    c = CustomClass()
+    time.sleep(5)
+    c.unsubscribe()
+    while True:
+        time.sleep(1)
